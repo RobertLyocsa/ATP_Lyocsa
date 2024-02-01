61fc0d2d41701d850011566e7510233605246d96
Robert Lyocsa <robert.kristian.lyocsa@gmail.com>
2024-02-01 23:08:10 +0100

    Please no more bullshit

---
+++ neprojektove-ulohy/InteliJ/mavenProjectCMD/src/test/java/sk/itvkurze/metis/zadanie20/TestMethods.java
@@ -0,0 +1,12 @@
+package sk.itvkurze.metis.zadanie20;
+
+public class TestMethods {
+    //creating a constructor
+
+    public TestMethods() {
+
+    }
+    public String returnMessage() {
+        return "Hello World";
+    }
+}
